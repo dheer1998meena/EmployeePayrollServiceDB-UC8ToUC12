f8936363084d9ff1429e2e888d9a12cee7e38bf9
dheer meena <dheer1998meena@gmail.com>
2020-11-03 08:17:25 +0530

    [Dheer] Add:Implemented the use cases for the entity framework exercise on the emp[loyee payroll table

--- UC_12_retrieving data from multiple tables.sql
+++ UC_12_retrieving data from multiple tables.sql
@@ -1,16 +1,17 @@
-/* Invoking the payroll services database */
+--UC12 retriving data using different tables
+--Invoking the payroll services database 
 use payroll_services;
-/* UC4 -- Retrieving all the records from the employee payroll table */
+--UC4Retrieving all the records from the employee payroll table
 select emp.EmpID, emp.EmpName, emp.StartDate, emp.gender, emp.phoneNumber, emp.address, 
 dept.department, dept.basic_pay, pay.deductions, pay.taxable_pay, pay.income_tax, pay.net_pay
  from employee_payroll emp, employee_department dept, payroll pay
  where emp.EmpId = dept.EmpId and dept.basic_pay = pay.basic_pay;
- /* UC5 -- Retrieving all the records from the employee payroll table for a particular employee*/
+ --UC5 Retrieving all the records from the employee payroll table for a particular employee.
 select emp.EmpId, emp.EmpName, emp.StartDate, emp.gender, emp.phoneNumber, emp.address, 
 dept.department, dept.basic_pay, pay.deductions, pay.taxable_pay, pay.income_tax, pay.net_pay
  from employee_payroll emp, employee_department dept, payroll pay
  where emp.EmpId= dept.EmpId and dept.basic_pay = pay.basic_pay and emp.EmpName = 'Terissa';
- /* UC5 -- Retrieving all the records from the employee payroll table for a employee joining date between a time frame*/
+ --UC5 Retrieving all the records from the employee payroll table for a employee joining date between a time fram
 select emp.EmpId, emp.EmpName, emp.StartDate, emp.gender, emp.phoneNumber, emp.address, 
 dept.department, dept.basic_pay, pay.deductions, pay.taxable_pay, pay.income_tax, pay.net_pay
  from employee_payroll emp, employee_department dept, payroll pay
